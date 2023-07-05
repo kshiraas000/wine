a519d16126f9eaf3f10e0ecbfa98c26364dd9732
Aashna Kshirsagar <ak16499@ad.georgiasouthern.edu>
2023-07-05 10:23:16 -0400

    added random forest and stratification

--- wine.py
+++ wine.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 # Specify the CSV file path
 csv_file = 'wine_combined_cleaned.csv'
@@ -15,7 +16,7 @@ X = data.iloc[:, :12].values
 y = data.iloc[:, -1].values
 
 # Splitting the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
 # Creating and training the decision tree classifier
 model = DecisionTreeClassifier()
@@ -26,4 +27,15 @@ predictions = model.predict(X_test)
 
 # Evaluating the model
 accuracy = np.mean(predictions == y_test)
-print(f"Accuracy: {accuracy}")
+print(f"Decision tree Accuracy (Stratified): {accuracy}")
+
+# Creating and training the random forest classifier
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+# Making predictions on the test set
+predictions = model.predict(X_test)
+
+# Evaluating the model
+accuracy = np.mean(predictions == y_test)
+print(f"Random Forest Accuracy (Stratified): {accuracy}")
