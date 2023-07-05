db014d3490a69595cfaa7bee5e89254510d0d51c
Aashna Kshirsagar <ak16499@ad.georgiasouthern.edu>
2023-07-05 10:09:05 -0400

    added environment file and ingest

---
+++ wine.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# Specify the CSV file path
+csv_file = 'wine_combined_cleaned.csv'
+
+# Read the CSV file into a Pandas DataFrame
+data = pd.read_csv(csv_file)
+
+# Print the DataFrame
+# print(data)
+
+data.head()
